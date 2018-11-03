bde8be85e0535153e1e1e42259eb3954b05638f2
Phillip Peloubet <Phillip.Peloubet@gmail.com>
2018-11-02 17:24:57 -0700

    Flow Control

---
+++ Flow Control/case_statement.rb
@@ -0,0 +1,25 @@
+a = 8
+
+answer = case
+  when a == 5
+    "a is 5"
+  when a == 6
+    "a is 6"
+  else 
+    "a is neither 5, nor 6"
+  end
+
+puts answer
+
+b = 6
+
+answer = case b
+  when 5
+    "b is 5"
+  when 6
+    "b is 6"
+  else 
+    "b is neither 5, nor 6"
+  end
+
+puts answer
